78a69b824ff9aefa49084077cce86232f9e73a95
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-05-15 11:07:34 +0100

    Added cookieData obj, setInt

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 // dom elements
 const resetBtn = document.getElementById("reset");
-const totalCookies = document.getElementById("total-cookies");
+const totalCookiesDisplay = document.getElementById("total-cookies");
 const incrementCookieBtn = document.getElementById("cookie-increment-btn");
 const cookiesPerSec = document.getElementById("cps");
+
+// Get data from local storage, if null create cookiedata object
+let cookieData = JSON.parse(localStorage.getItem("cookieGameData")) || {
+  totalCookies: 1,
+  cookieClickValue: 1,
+  cookiesPerSec: 1,
+};
+
+// Callbacks
+
+// Callback for setInterval
+const incrementTotalCookies = () => {
+  cookieData.totalCookies += cookieData.cookiesPerSec;
+  totalCookiesDisplay.textContent = `${cookieData.totalCookies}`;
+  localStorage.setItem("cookieGameData", JSON.stringify(cookieData));
+};
+
+// set interval
+setInterval(incrementTotalCookies, 1000);
