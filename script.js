b722136e7f015a434fe97f932db4d73180ede785
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-05-15 10:51:59 +0100

    Added dom elements

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 // dom elements
+const resetBtn = document.getElementById("reset");
+const totalCookies = document.getElementById("total-cookies");
+const incrementCookieBtn = document.getElementById("cookie-increment-btn");
+const cookiesPerSec = document.getElementById("cps");
